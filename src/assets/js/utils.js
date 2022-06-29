3bdc3f330f7b86bebb742338ea43d592166632ce
Jérémy Saint-Prix <jeremy.saintprix@gmail.com>
2022-06-29 19:02:13 +0200

    v-scroll & drag behaviours, missing inertia on drag (#7)

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -56,18 +56,9 @@ export function vhCalc() {
 	})
 }
 
-
-export function getDeviceType() {
-  const ua = navigator.userAgent;
-  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-    return "tablet"
-  }
-  if (
-    /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
-      ua
-    )
-  ) {
-    return "mobile"
-  }
-  return "desktop"
+export const deviceType = {
+	isMobile: /Android|iPhone|iPad|iPod/i.test(navigator.userAgent),
+	isTablet: /(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(navigator.userAgent),
+	isSafari: /^((?!Chrome|Android).)*Safari/i.test(navigator.userAgent),
+	isTouch: 'ontouchstart' in window,
 }
